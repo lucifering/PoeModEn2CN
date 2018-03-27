e560960f68d067a06a1493f454e87258fe93cd84
HouWeiBin <1264534037@qq.com>
2018-03-27 13:35:10 +0800

    [ADD] 3.2词缀+poe.ninja/builds支持

--- myscript.js
+++ myscript.js
@@ -1,20 +1,36 @@
-
 function click(e) {
-	  chrome.tabs.executeScript(null, {file: "Item.js"});
-	    chrome.tabs.executeScript(null, {file: "ExplicitModjson.js"});
-		  chrome.tabs.executeScript(null, {file: "Gem.js"});
-		  chrome.tabs.executeScript(null, {file: "Jewels.js"});
-		   chrome.tabs.executeScript(null, {file: "mods.js"});
-	 chrome.tabs.executeScript(null, {file: "try2cn.js"});
 
-	  window.close();
-	}
+    chrome.tabs.executeScript(null, {
+        file: "nija.js"
+    });
 
-document.addEventListener('DOMContentLoaded', function () {
-	  var divs = document.querySelectorAll('div');
-	  for (var i = 0; i < divs.length; i++) {
-	    divs[i].addEventListener('click', click);
-	  }
+    chrome.tabs.executeScript(null, {
+        file: "Item.js"
+    });
+    chrome.tabs.executeScript(null, {
+        file: "ExplicitModjson.js"
+    });
+    chrome.tabs.executeScript(null, {
+        file: "Gem.js"
+    });
+    chrome.tabs.executeScript(null, {
+        file: "Unique.js"
+    });
+    chrome.tabs.executeScript(null, {
+        file: "mods.js"
+    });
+    chrome.tabs.executeScript(null, {
+        file: "try2cn.js"
+    });
+
+    window.close();
+}
+
+document.addEventListener('DOMContentLoaded',
+function() {
+    var divs = document.querySelectorAll('div');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', click);
+    }
 
 });
-
